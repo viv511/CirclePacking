e7b8f33e3f59e9bd4136ae2c853474d46bd7fdb5
Vivek Mehta <66559749+viv511@users.noreply.github.com>
2024-06-19 17:11:25 -0400

    Progress! - got basic animating and circle moving working
    - need to figure out how to clear old circles
    - figure out new circle gen on switching images

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ var generatingCircle = false;
 
 //Change if you want to do image detection or not
 var isBadApple = true;
+const growthConst = 4
 
 class Circle {
   constructor(x, y, r) {
@@ -15,16 +16,16 @@ class Circle {
   
   show() {
     stroke(255)
-    noFill()
+    fill(0);
     strokeWeight(3)
-    ellipse(this.x, this.y, this.r * 2, this.r * 2);
+    circle(this.x, this.y, this.r * 2);
   } 
 
   changeSize() {
     if(!this.collision()) {
       if(!this.touchingOtherCircle()) {
         if(!this.touchingWhite()) {
-          this.r += 2;
+          this.r += growthConst;
           generatingCircle = true;
         }
         else {
@@ -80,13 +81,10 @@ class Circle {
 
 var circles = []
 
-//500, 500 
 let canvasWidth = 600;
 let canvasHeight = 450;
 
-let img;
-var xCoords = []
-var yCoords = []
+var img;
 
 function findCoordinate(x, y) {
   for(var i=0; i<xCoords.length; i++) {
@@ -99,44 +97,17 @@ function findCoordinate(x, y) {
 }
 
 function preload() {
-  img = loadImage("data/videoFrames/1ezgif-frame-001.jpg")
+  img = loadImage("data/videoFrames/3ezgif-frame-001.jpg")
+  generateCounts();
 }
 
 function setup() {
   buttonHandling();
   createCanvas(canvasWidth, canvasHeight);
-    
-  handleImage();
-
   colorMode(HSB, 360, 100, 100, 100);
   ellipseMode(CENTER);
-}
 
-function handleImage() {
-  xCoords = [];
-  yCoords = [];
-
-  img.loadPixels();
-
-  let xCoordinate = 0;
-  let yCoordinate = 0;
-
-  for (let i=0; i<img.pixels.length; i+=4) {
-    xCoordinate++;
-    if(xCoordinate == canvasWidth) {
-      xCoordinate = 0;
-      yCoordinate++;
-    }
-
-    let val = (img.pixels[i]+img.pixels[i+1]+img.pixels[i+2]) / 3;
-    if(val < 10) {
-      //BLACK!
-      xCoords.push(xCoordinate);
-      yCoords.push(yCoordinate);
-    }
-  }
-
-  updatePixels();
+  triggerImage();
 }
 
 function startProject() {
@@ -154,83 +125,119 @@ function buttonHandling() {
 
 function resetCircles() {
   circles = []
-  frameCount = 0;
-  setNum = 1;
+  frameNumber = [];
+  setNumber = [];
+  generateCounts();
 }
 
-const timeToTrigger = 0.1;
+var frameNumber = [];
+var setNumber = [];
 
-var setNum = 1;
-function triggerFrame(frame) {
-  frame = frame - 150 * (setNum - 1)
-
-  if((frame == 47) && setNum == 8) {
-    resetCircles();
+function generateCounts() {
+  for(var i=1; i<=7; i++) {
+    for(var j=1; j<=150; j++) {
+      frameNumber.push(j);
+      setNumber.push(i);
+    }
   }
+  for(var j=1; j<=47; j++) {
+    frameNumber.push(j);
+    setNumber.push(8);
+  }
+}
+
+var imageNumber = 50; //SWITCH BACK TO 0
+
+
+var xCoords = []
+var yCoords = []
+
+function handleImage(img) {
+  background(img);
+  xCoords = [];
+  yCoords = [];
+
+  img.loadPixels();
+
+  let xCoordinate = 0;
+  let yCoordinate = 0;
+
+  for (let i=0; i<img.pixels.length; i+=4) {
+    xCoordinate++;
+    if(xCoordinate == canvasWidth) {
+      xCoordinate = 0;
+      yCoordinate++;
+    }
+
+    let val = (img.pixels[i]+img.pixels[i+1]+img.pixels[i+2]) / 3;
+    if(val < 100) {
+      //BLACK!
+      xCoords.push(xCoordinate);
+      yCoords.push(yCoordinate);
+    }
+  }
+
+  console.log(xCoords, yCoords)
+  updatePixels();
+}
+
+function triggerImage() {
+  var frame = frameNumber[imageNumber];
+  var setNum = setNumber[imageNumber];
 
   if(frame < 10) {
-    img = loadImage("data/videoFrames/" + setNum + "ezgif-frame-00" + frame + ".jpg")
+    loadImage("data/videoFrames/" + setNum + "ezgif-frame-00" + frame + ".jpg", handleImage)
   }
   else if(frame < 100) {
-    img = loadImage("data/videoFrames/" + setNum + "ezgif-frame-0" + frame + ".jpg")
+    loadImage("data/videoFrames/" + setNum + "ezgif-frame-0" + frame + ".jpg", handleImage)
   }
   else {
-    img = loadImage("data/videoFrames/" + setNum + "ezgif-frame-" + frame + ".jpg")
-  }
-  
-  handleImage();
-
-  if(frame == 150) {
-    setNum++;
+    loadImage("data/videoFrames/" + setNum + "ezgif-frame-" + frame + ".jpg", handleImage)
   }
 
-  console.log(setNum, frame)
+  imageNumber++;
+  if(imageNumber == frameNumber.length) {
+    imageNumber = 0;
+  }
+} 
+
+
+//DOESNT WORK PROPERLY -- FIX IF STATEMENTS?
+function removeIncorrectCircles() {
+  for (let i=0; i<circles.length; i++) {
+    // if(circles[i].touchingOtherCircle()) {
+    //   circles.splice(i, 1);
+    //   console.log("other circle")
+    // }
+    // if(circles[i].collision()) {
+    //   circles.splice(i, 1);
+    //   console.log("collision")
+    //   i--;
+    // }
+    if(circles[i].touchingWhite()) {
+      circles.splice(i, 1);
+      console.log("white")
+      i--;
+    }
+
+  }
 }
 
-var lastFrameChangeTime = 0;
 
 function draw() {
-  if(frameCount % 2 == 0) {
-    triggerFrame(frameCount / 2);
+  frameRate(120);
+  // background(0);
+
+
+  if(circles.length > 100) {
+    removeIncorrectCircles();
+    // circles = [];
+    triggerImage();
   }
-  // // Check if enough time has passed since the last frame change
-  // if(millis() - lastFrameChangeTime > timeToTrigger * 1000) {
-  //   lastFrameChangeTime = millis();
 
-  //   currentFrame++;
-  //   triggerFrame(1 + floor(frameCount / 60) - (150 * (setNum - 1)));
-  // }
+  // setInterval(triggerImage, 2000);
 
-  background(img);
-  frameRate(60);
-
-  // for (var i=0; i<xCoords.length; i++) {
-  //   stroke(255)
-  //   noFill()
-  //   strokeWeight(3)
-  //   ellipse(xCoords[i], yCoords[i], 3, 3);
-  // }
-
-  // Gradient Code ~ https://www.youtube.com/watch?v=-MUOweQ6wac
-  let angle = map(frameCount % 360, 0, 360, 0, TWO_PI) * 2; //Speed up by 2
-  let centerX = canvasWidth / 2;
-  let centerY = canvasHeight / 2;
-  let lineLength = 250;
-
-  let gStartX = centerX + cos(angle) * lineLength;
-  let gStartY = centerY + sin(angle) * lineLength;
-
-  let gEndX = centerX + cos(angle + PI) * lineLength;
-  let gEndY = centerY + sin(angle + PI) * lineLength;
-
-  let gradient = drawingContext.createLinearGradient(
-    gStartX, gStartY, gEndX, gEndY
-  );
-
-  gradient.addColorStop(0, color(310, 100, 100, 100));
-  gradient.addColorStop(1, color(250, 100, 100, 100));
-  // drawingContext.fillStyle = gradient;
-  drawingContext.strokeStyle = gradient;
+  gradientGeneration();
 
   var doneIndex;
   if(!generatingCircle) {
@@ -240,10 +247,44 @@ function draw() {
     xCoords.splice(doneIndex, 1)
     yCoords.splice(doneIndex, 1)
   }
+  circleLoop();
+  
+}
 
+function gradientGeneration() {
+    // Gradient Code ~ https://www.youtube.com/watch?v=-MUOweQ6wac
+    let angle = map(frameCount % 360, 0, 360, 0, TWO_PI) * 2; //Speed up by 2
+    let centerX = canvasWidth / 2;
+    let centerY = canvasHeight / 2;
+    let lineLength = 250;
+  
+    let gStartX = centerX + cos(angle) * lineLength;
+    let gStartY = centerY + sin(angle) * lineLength;
+  
+    let gEndX = centerX + cos(angle + PI) * lineLength;
+    let gEndY = centerY + sin(angle + PI) * lineLength;
+  
+    let gradient = drawingContext.createLinearGradient(
+      gStartX, gStartY, gEndX, gEndY
+    );
+  
+    gradient.addColorStop(0, color(310, 100, 100, 100));
+    gradient.addColorStop(1, color(250, 100, 100, 100));
+    // drawingContext.fillStyle = gradient;
+    drawingContext.strokeStyle = gradient;
+}
+
+function circleLoop() {
   for (let i = 0; i < circles.length; i++) {
+
+    //CHANGE THIS IF U WANT ANIMATED
     circles[i].show();
     circles[i].changeSize();
+
+    // circles[i].changeSize();
+    // if(!generatingCircle) {
+    //   circles[i].show();
+    // }
   }
 }
 
@@ -271,3 +312,41 @@ function drawCircle() {
 
   return index;
 }
+
+// var setNum = 1;
+
+// function triggerFrame(frame) {
+//   frame = frame - 150 * (setNum - 1)
+
+//   if((frame == 47) && setNum == 8) {
+//     resetCircles();
+//   }
+
+//   if(frame < 10) {
+//     img = loadImage("data/videoFrames/" + setNum + "ezgif-frame-00" + frame + ".jpg")
+//   }
+//   else if(frame < 100) {
+//     img = loadImage("data/videoFrames/" + setNum + "ezgif-frame-0" + frame + ".jpg")
+//   }
+//   else {
+//     img = loadImage("data/videoFrames/" + setNum + "ezgif-frame-" + frame + ".jpg")
+//   }
+  
+//   handleImage();
+
+//   if(frame == 150) {
+//     setNum++;
+//   }
+
+//   console.log(setNum, frame)
+// }
+//   if(frameCount % 2 == 0) {
+//     triggerFrame(frameCount / 2);
+//   }
+//   // // Check if enough time has passed since the last frame change
+//   // if(millis() - lastFrameChangeTime > timeToTrigger * 1000) {
+//   //   lastFrameChangeTime = millis();
+
+//   //   currentFrame++;
+//   //   triggerFrame(1 + floor(frameCount / 60) - (150 * (setNum - 1)));
+//   // }
